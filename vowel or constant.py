dbeda7acca7892b9223d8c73e08482e251b8c390
bemelin <45841648+bemelin@users.noreply.github.com>
2018-12-14 15:18:35 +0530

    Create vowel or constant.py

---
+++ vowel or constant.py
@@ -0,0 +1,7 @@
+1=input("input a letter of the alphabet:")
+if('a','e','i','o','u'):
+    print("%s is a vowel."%1)
+elif1=='y':
+     print("sometime letter y stand for vowel,sometime stand for consonant.")
+else:
+     print("%s is a constant."%1)
